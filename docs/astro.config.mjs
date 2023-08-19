fd53655018ae1243bafdb838278707e6e68921e2
HeisenBerg? <edwin_uestc@163.com>
2023-08-20 03:08:25 +0800

    Update astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
 	site,
 	integrations: [
 		starlight({
-			title: 'Starlight',
+			title: 'understanding astro',
 			logo: {
 				light: '/src/assets/logo-light.svg',
 				dark: '/src/assets/logo-dark.svg',
@@ -52,6 +52,164 @@ export default defineConfig({
 			customCss: process.env.NO_GRADIENTS ? [] : ['./src/assets/landing.css'],
 			locales,
 			sidebar: [
+				{
+					label: 'understanding astro',
+					translations: {
+						de: 'Beginne hier',
+						es: 'Comienza aqui',
+						ja: 'ここからはじめる',
+						fr: 'Commencez ici',
+						it: 'Inizia qui',
+						zh: '从这里开始',
+						'pt-BR': 'Comece Aqui',
+					},
+					items: [
+						{
+							label: 'preface',
+							link: 'preface',
+							translations: {
+								de: 'Erste Schritte',
+								es: 'Empezando',
+								ja: '入門',
+								fr: 'Mise en route',
+								it: 'Iniziamo',
+								zh: '开始使用',
+								'pt-BR': 'Introdução',
+							},
+						},
+						{
+							label: 'chapter1',
+							link: 'ch1',
+							translations: {
+								de: 'Erste Schritte',
+								es: 'Empezando',
+								ja: '入門',
+								fr: 'Mise en route',
+								it: 'Iniziamo',
+								zh: '开始使用',
+								'pt-BR': 'Introdução',
+							},
+						},
+						{
+							label: 'chapter2',
+							link: 'ch2',
+							translations: {
+								de: 'Erste Schritte',
+								es: 'Empezando',
+								ja: '入門',
+								fr: 'Mise en route',
+								it: 'Iniziamo',
+								zh: '开始使用',
+								'pt-BR': 'Introdução',
+							},
+						},
+						{
+							label: 'chapter3',
+							link: 'ch3',
+							translations: {
+								de: 'Erste Schritte',
+								es: 'Empezando',
+								ja: '入門',
+								fr: 'Mise en route',
+								it: 'Iniziamo',
+								zh: '开始使用',
+								'pt-BR': 'Introdução',
+							},
+						},
+						{
+							label: 'chapter4',
+							link: 'ch4',
+							translations: {
+								de: 'Erste Schritte',
+								es: 'Empezando',
+								ja: '入門',
+								fr: 'Mise en route',
+								it: 'Iniziamo',
+								zh: '开始使用',
+								'pt-BR': 'Introdução',
+							},
+						},
+						{
+							label: 'chapter1',
+							link: 'ch1',
+							translations: {
+								de: 'Erste Schritte',
+								es: 'Empezando',
+								ja: '入門',
+								fr: 'Mise en route',
+								it: 'Iniziamo',
+								zh: '开始使用',
+								'pt-BR': 'Introdução',
+							},
+						},
+						{
+							label: 'chapter5',
+							link: 'ch5',
+							translations: {
+								de: 'Erste Schritte',
+								es: 'Empezando',
+								ja: '入門',
+								fr: 'Mise en route',
+								it: 'Iniziamo',
+								zh: '开始使用',
+								'pt-BR': 'Introdução',
+							},
+						},
+						{
+							label: 'chapter6',
+							link: 'ch6',
+							translations: {
+								de: 'Erste Schritte',
+								es: 'Empezando',
+								ja: '入門',
+								fr: 'Mise en route',
+								it: 'Iniziamo',
+								zh: '开始使用',
+								'pt-BR': 'Introdução',
+							},
+						},
+						{
+							label: 'chapter7',
+							link: 'ch7',
+							translations: {
+								de: 'Erste Schritte',
+								es: 'Empezando',
+								ja: '入門',
+								fr: 'Mise en route',
+								it: 'Iniziamo',
+								zh: '开始使用',
+								'pt-BR': 'Introdução',
+							},
+						},
+						{
+							label: 'chapter8',
+							link: 'ch8',
+							translations: {
+								de: 'Erste Schritte',
+								es: 'Empezando',
+								ja: '入門',
+								fr: 'Mise en route',
+								it: 'Iniziamo',
+								zh: '开始使用',
+								'pt-BR': 'Introdução',
+							},
+						},
+						{
+							label: 'conclusion',
+							link: 'conclusion',
+							translations: {
+								de: 'Erste Schritte',
+								es: 'Empezando',
+								ja: '入門',
+								fr: 'Mise en route',
+								it: 'Iniziamo',
+								zh: '开始使用',
+								'pt-BR': 'Introdução',
+							},
+						},
+
+					],
+				},
 				{
 					label: 'Start Here',
 					translations: {
